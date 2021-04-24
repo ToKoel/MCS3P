fb1fc5c014d303c34b58f49969492ec22cc3c1c6
Tobias Köhler <t.koehler@fz-juelich.de>
2021-04-24 12:08:16 +0200

    Initial Commit

---
+++ MCS3P/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MCS3P
+//
+//  Created by Tobias Köhler on 24.04.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
