0c94a6d7a63364f55a8f5e115ef6034795c721f6
Tobias Köhler <t.koehler@fz-juelich.de>
2021-06-27 21:47:57 +0200

    cleaning up main.cpp moving test functions to testing.cpp

---
+++ MCS3P/testing.hpp
@@ -0,0 +1,28 @@
+//
+//  testing.hpp
+//  MCS3P
+//
+//  Created by Tobias Köhler on 27.06.21.
+//
+
+#ifndef testing_hpp
+#define testing_hpp
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "Crystal.hpp"
+#include "randNumGenerator.hpp"
+
+struct retVals {
+    double E_a, E_e, E_z, E_d;
+};
+
+void sigma_tests(double sigma);
+void dipole_calcs(bool APB, int D_min, int D_max);
+void relaxation_test(double size, double sigma, int steps);
+void calc_dipole_field();
+retVals particle_configurations_test(std::string mode, std::string infile, std::string outfile, double c);
+
+#endif /* testing_hpp */
